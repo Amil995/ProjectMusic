ae4512fd1df2eba072120f1b56aa4be65a4cdc49
Amil995 <121118369+Amil995@users.noreply.github.com>
2023-04-23 23:35:02 +0400

    Update javas.js

--- js/javas.js
+++ js/javas.js
@@ -6,22 +6,38 @@ btn.addEventListener("click", () => {
     men.classList.toggle("active");
 } );
 
+const cards = document.querySelector(".cards");
+const header = document.querySelector(".header");
 const close = document.querySelector(".close-button");
 const modal = document.querySelector(".window");
 const show = document.querySelector(".text");
 const send = document.querySelector(".send-button");
+const modal1 = document.querySelector(".modal");
+
 
 show.addEventListener("click", () => {
     modal.style.display = "flex";
-    console.log("works")
+    console.log("works");
+    cards.classList.toggle("active");
+    header.classList.toggle("active");
+    modal1.classList.toggle("active");
 } );
+
 
 close.addEventListener("click", () => {
    modal.style.display = "none";
+   cards.classList.toggle("close");
+   header.classList.toggle("close");
+   modal1.classList.toggle("close");
 });
+
+
 
 send.addEventListener("click", () => {
     modal.style.display = "none";
+    cards.classList.toggle("close");
+    header.classList.toggle("close");
+    modal1.classList.toggle("close");
 });
 
 function myFunction() {
